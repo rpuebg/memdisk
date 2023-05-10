d97ecae6397cf7ac1cda8a083bc9b397b8818c5e
rpuebg <rpuebg@hotmail.com>
2023-05-10 20:41:10 +0200

    Update memdisk.sh

--- memdisk.sh
+++ memdisk.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Obtener el uso de memoria en porcentaje
+memory_usage=$(free | awk '/Mem/ {printf("%3.1f%%", $3/$2*100)}')
+# Obtener el uso del disco en porcentaje
+disk_usage=$(df -h | awk '/^\/dev\/sda1/ {print $5}')
+# Mostrar el uso de memoria y disco en la consola
+echo "Uso de memoria: $memory_usage"
+echo "Uso del disco: $disk_usage"
 
